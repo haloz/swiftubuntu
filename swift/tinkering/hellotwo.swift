2b9ccaaa36c619137fe723a06edf9a08ab67470d
haloz <haloz@gmx.net>
2016-12-10 22:59:36 +0100

    map sorted func in func func as parameter

--- swift/tinkering/hellotwo.swift
+++ swift/tinkering/hellotwo.swift
@@ -71,3 +71,66 @@ func average(numbers: Float...) -> Float {
 }
 print(String(average(numbers: 1.5, 2.3, 8.1, 2.4)))
 
+// func in func
+func returnFifteen() -> Int {
+	var y = 10
+	func add() {
+		y += 5
+	}
+	add()
+	return y
+}
+print(returnFifteen())
+
+// function returning another function
+func makeIncrementer() -> ((Int) -> Int) {
+	func addOne(number: Int) -> Int {
+		return 1 + number
+	}
+	return addOne
+}
+var incrementer = makeIncrementer()
+print(incrementer(7)) // 8
+
+// function as argument
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+	for item in list {
+		if condition(item) {
+			return true
+		}
+	}
+	return false
+}
+func lessThanTen(number: Int) -> Bool {
+	return number < 10
+}
+var numbers = [20, 19, 9, 12]
+// 2nd param is a func
+print(hasAnyMatches(list: numbers, condition: lessThanTen))
+// note: reminds of javascript closures, funcs are just blocks of
+// code that can be called later. there're also unnamed closures:
+var newNumbers = numbers.map({
+	(number: Int) -> Int in
+	let result = 3 * number
+	return result
+})
+print(newNumbers) // 60, 57, 27, 36
+
+// shorter:
+print(numbers.map({ number in 3 * number}))
+
+// zero for all odd numbers
+var zeroodds = numbers.map({
+	(number: Int) -> Int in
+	if number % 2 == 0 {
+		return number
+	}
+	return 0
+})
+print(zeroodds) // 20, 0, 0, 12
+
+// shorter & with ternary conditional operator
+print(numbers.map({ number in number % 2 == 0 ? number : 0}))
+
+// can refer to parameters by number, not by name. pretty neat :)
+print(numbers.sorted { $0 > $1 })
