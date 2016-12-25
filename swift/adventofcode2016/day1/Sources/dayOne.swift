b81101be874e345e3d4a1575d5b64ff61c6f2282
Sebastian Lange <sebastian.lange@lovoo.com>
2016-12-25 11:55:35 +0100

    advent of code 2016 day 1 taxicab distance Part 2

--- swift/adventofcode2016/day1/Sources/dayOne.swift
+++ swift/adventofcode2016/day1/Sources/dayOne.swift
@@ -48,4 +48,110 @@ struct Taxicab {
         
         return abs(0-travelledEast) + abs(0-travelledNorth)
     }
+    
+    static func twiceVisited(_ instructions:String) -> Int {
+        var travelledNorth = 0
+        var travelledEast = 0
+        var pointing = "north"
+        var visitedLocations = [String]()
+        
+        visitedLocations.append("N0E0")
+        
+        for step in instructions.components(separatedBy: ", ") {
+            let indexFirstChar = step.index(step.startIndex, offsetBy: 1)
+            let direction = step.substring(to: indexFirstChar)
+            let distance = step.substring(from: indexFirstChar)
+            
+            if pointing == "north" {
+                if direction == "L" {
+                    for _ in (1...Int(distance)!) {
+                        travelledEast -= 1
+                        let visited = "E\(travelledEast)N\(travelledNorth)"
+                        if visitedLocations.contains(visited) {
+                            return abs(0-travelledEast) + abs(0-travelledNorth)
+                        }
+                        visitedLocations.append(visited)
+                    }
+                    pointing = "west"
+                } else if direction == "R" {
+                    for _ in (1...Int(distance)!) {
+                        travelledEast += 1
+                        let visited = "E\(travelledEast)N\(travelledNorth)"
+                        if visitedLocations.contains(visited) {
+                            return abs(0-travelledEast) + abs(0-travelledNorth)
+                        }
+                        visitedLocations.append(visited)
+                    }
+                    pointing = "east"
+                }
+            } else if pointing == "west" {
+                if direction == "L" {
+                    for _ in (1...Int(distance)!) {
+                        travelledNorth -= 1
+                        let visited = "E\(travelledEast)N\(travelledNorth)"
+                        if visitedLocations.contains(visited) {
+                            return abs(0-travelledEast) + abs(0-travelledNorth)
+                        }
+                        visitedLocations.append(visited)
+                    }
+                    pointing = "south"
+                } else if direction == "R" {
+                    for _ in (1...Int(distance)!) {
+                        travelledNorth += 1
+                        let visited = "E\(travelledEast)N\(travelledNorth)"
+                        if visitedLocations.contains(visited) {
+                            return abs(0-travelledEast) + abs(0-travelledNorth)
+                        }
+                        visitedLocations.append(visited)
+                    }
+                    pointing = "north"
+                }
+            } else if pointing == "east" {
+                if direction == "L" {
+                    for _ in (1...Int(distance)!) {
+                        travelledNorth += 1
+                        let visited = "E\(travelledEast)N\(travelledNorth)"
+                        if visitedLocations.contains(visited) {
+                            return abs(0-travelledEast) + abs(0-travelledNorth)
+                        }
+                        visitedLocations.append(visited)
+                    }
+                    pointing = "north"
+                } else if direction == "R" {
+                    for _ in (1...Int(distance)!) {
+                        travelledNorth -= 1
+                        let visited = "E\(travelledEast)N\(travelledNorth)"
+                        if visitedLocations.contains(visited) {
+                            return abs(0-travelledEast) + abs(0-travelledNorth)
+                        }
+                        visitedLocations.append(visited)
+                    }
+                    pointing = "south"
+                }
+            } else if pointing == "south" {
+                if direction == "L" {
+                    for _ in (1...Int(distance)!) {
+                        travelledEast += 1
+                        let visited = "E\(travelledEast)N\(travelledNorth)"
+                        if visitedLocations.contains(visited) {
+                            return abs(0-travelledEast) + abs(0-travelledNorth)
+                        }
+                        visitedLocations.append(visited)
+                    }
+                    pointing = "east"
+                } else if direction == "R" {
+                    for _ in (1...Int(distance)!) {
+                        travelledEast -= 1
+                        let visited = "E\(travelledEast)N\(travelledNorth)"
+                        if visitedLocations.contains(visited) {
+                            return abs(0-travelledEast) + abs(0-travelledNorth)
+                        }
+                        visitedLocations.append(visited)
+                    }
+                    pointing = "west"
+                }
+            }
+        }
+        return 0
+    }
 }
--- swift/adventofcode2016/day1/Tests/dayOneTests/dayOneTests.swift
+++ swift/adventofcode2016/day1/Tests/dayOneTests/dayOneTests.swift
@@ -20,6 +20,15 @@ class dayOneTests: XCTestCase {
         let puzzleInput = "R4, R3, L3, L2, L1, R1, L1, R2, R3, L5, L5, R4, L4, R2, R4, L3, R3, L3, R3, R4, R2, L1, R2, L3, L2, L1, R3, R5, L1, L4, R2, L4, R3, R1, R2, L5, R2, L189, R5, L5, R52, R3, L1, R4, R5, R1, R4, L1, L3, R2, L2, L3, R4, R3, L2, L5, R4, R5, L2, R2, L1, L3, R3, L4, R4, R5, L1, L1, R3, L5, L2, R76, R2, R2, L1, L3, R189, L3, L4, L1, L3, R5, R4, L1, R1, L1, L1, R2, L4, R2, L5, L5, L5, R2, L4, L5, R4, R4, R5, L5, R3, L1, L3, L1, L1, L3, L4, R5, L3, R5, R3, R3, L5, L5, R3, R4, L3, R3, R1, R3, R2, R2, L1, R1, L3, L3, L3, L1, R2, L1, R4, R4, L1, L1, R3, R3, R4, R1, L5, L2, R2, R3, R2, L3, R4, L5, R1, R4, R5, R4, L4, R1, L3, R1, R3, L2, L3, R1, L2, R3, L3, L1, L3, R4, L4, L5, R3, R5, R4, R1, L2, R3, R5, L5, L4, L1, L1"
         XCTAssertEqual(Taxicab.distanceToHQ(puzzleInput), 288)
     }
+    
+    func testLocationRevisitExample() {
+        XCTAssertEqual(Taxicab.twiceVisited("R8, R4, R4, R8"), 4)
+    }
+    
+    func testLocationRevisitMyInstructions() {
+        let puzzleInput = "R4, R3, L3, L2, L1, R1, L1, R2, R3, L5, L5, R4, L4, R2, R4, L3, R3, L3, R3, R4, R2, L1, R2, L3, L2, L1, R3, R5, L1, L4, R2, L4, R3, R1, R2, L5, R2, L189, R5, L5, R52, R3, L1, R4, R5, R1, R4, L1, L3, R2, L2, L3, R4, R3, L2, L5, R4, R5, L2, R2, L1, L3, R3, L4, R4, R5, L1, L1, R3, L5, L2, R76, R2, R2, L1, L3, R189, L3, L4, L1, L3, R5, R4, L1, R1, L1, L1, R2, L4, R2, L5, L5, L5, R2, L4, L5, R4, R4, R5, L5, R3, L1, L3, L1, L1, L3, L4, R5, L3, R5, R3, R3, L5, L5, R3, R4, L3, R3, R1, R3, R2, R2, L1, R1, L3, L3, L3, L1, R2, L1, R4, R4, L1, L1, R3, R3, R4, R1, L5, L2, R2, R3, R2, L3, R4, L5, R1, R4, R5, R4, L4, R1, L3, R1, R3, L2, L3, R1, L2, R3, L3, L1, L3, R4, L4, L5, R3, R5, R4, R1, L2, R3, R5, L5, L4, L1, L1"
+        XCTAssertEqual(Taxicab.twiceVisited(puzzleInput), 111) // "E12N-99"
+    }
 
 
     static var allTests : [(String, (dayOneTests) -> () throws -> Void)] {
@@ -27,7 +36,8 @@ class dayOneTests: XCTestCase {
             ("testExample1", testExample1),
             ("testExample2", testExample2),
             ("testExample3", testExample3),
-            ("testMyInstructions", testMyInstructions)
+            ("testMyInstructions", testMyInstructions),
+            ("testLocationRevisitMyInstructions", testLocationRevisitMyInstructions)
         ]
     }
 }
